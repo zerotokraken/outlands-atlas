36aa168064e396382d4a3f8bc6b09295f955bc68
zerotokraken <ztk.cmg@gmail.com>
2025-01-19 01:54:41 -0500

    Update webpack config and fix Node.js version for Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -26,24 +26,21 @@ export default {
     publicPath: '/',
     clean: true,
   },
-    plugins: [
+  plugins: [
     new CopyPlugin({
       patterns: [
         { from: 'index.html', to: '.' },
-        { from: 'src/floors', to: 'src/floors' },
         { from: 'src/icons', to: 'src/icons' },
         { from: 'src/images', to: 'src/images' },
         { from: 'src/css', to: 'src/css' },
         { from: 'src/json', to: 'src/json' },
-        { from: 'src/floors/floor-1/tiles', to: 'src/floors/floor-1/tiles' },
-        { from: 'src/floors/floor-2/tiles', to: 'src/floors/floor-2/tiles' },
-        { from: 'src/floors/floor-3/tiles', to: 'src/floors/floor-3/tiles' },
-        { from: 'src/floors/floor-4/tiles', to: 'src/floors/floor-4/tiles' },
-        { from: 'src/floors/floor-5/tiles', to: 'src/floors/floor-5/tiles' },
-        { from: 'src/floors/floor-6/tiles', to: 'src/floors/floor-6/tiles' },
-        { from: 'src/floors/floor-6.5/tiles', to: 'src/floors/floor-6.5/tiles' },
-        { from: 'src/floors/floor-7/tiles', to: 'src/floors/floor-7/tiles' },
-        { from: 'src/floors/floor-8/tiles', to: 'src/floors/floor-8/tiles' },
+        { 
+          from: 'src/floors/**/required_tiles.json',
+          to: ({ context, absoluteFilename }) => {
+            const relativePath = path.relative(context, absoluteFilename);
+            return relativePath;
+          }
+        }
       ],
     }),
   ],
